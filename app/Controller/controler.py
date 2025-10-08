8e009df9777f7b9e1c0091ed806302ac23f60032
Ng20170201 <nikolag857@gmail.com>
2025-10-08 11:53:55 +0200

    sest version on v2025.14.0.

--- app/Controller/controler.py
+++ app/Controller/controler.py
@@ -9,20 +9,3 @@ router = APIRouter()
 @router.get("/", summary="Root endpoint")
 async def read_root():
     return {"message": "Hello, FastAPI with Swagger and MongoDB!"}
-
-
-@router.get("/items/", summary="List items")
-async def list_items(request: Request):
-    db = request.app.state.mongodb
-    cursor = db.items.find()
-    items = []
-    async for doc in cursor:
-        # convert ObjectId to str for JSON serialization
-        doc["_id"] = str(doc.get("_id"))
-        items.append(doc)
-    return items
-
-
-@router.get("/items/{item_id}", summary="Read an item")
-async def read_item(item_id: str, request: Request):
-    return 1
--- app/main.py
+++ app/main.py
@@ -30,4 +30,5 @@ async def shutdown_db_client():
 
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
